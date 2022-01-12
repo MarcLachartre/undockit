0dc7248b231f2bf41617d6c98fd1c18e50bc79ef
marclachartre <marc.lachartre@gmail.com>
2022-01-12 11:17:02 +0100

    flame sprite fix for mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8cdae4812f44d0af"; }
+/******/ 		__webpack_require__.h = function() { return "aa73a730dcf9c2d7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
